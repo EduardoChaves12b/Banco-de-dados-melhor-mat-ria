70bc0da9513572ef7b4877e67e8c5059b838ace1
EduardoChaves12b <102633105+EduardoChaves12b@users.noreply.github.com>
2023-09-26 20:36:15 -0300

    Update listaExercicios02.sql
    exercicio 2

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -9,3 +9,20 @@ DELIMITER ;
 CALL sp_ListarAutores();
 
 -----------------------------------------------------------------------------
+
+-- Exercício 2
+
+DELIMITER //
+CREATE PROCEDURE sp_LivrosPorCategoria(IN categoriaNome VARCHAR(100))
+BEGIN
+    SELECT Livro.Titulo
+    FROM Livro
+    INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+    WHERE Categoria.Nome = categoriaNome;
+END;
+//
+DELIMITER ;
+
+-----------------------------------------------------------------------------
+
+
